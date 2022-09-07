2c363513e1fb4e97f545f9e2fd0ed223bceedf15
QilunBu <92778793+QilunBu@users.noreply.github.com>
2022-09-07 20:04:14 +1000

    completed serverside validation

--- src/main/java/com/qilun/expensemanager/controller/ExpenseController.java
+++ src/main/java/com/qilun/expensemanager/controller/ExpenseController.java
@@ -4,14 +4,17 @@ import com.qilun.expensemanager.dto.ExpenseDTO;
 import com.qilun.expensemanager.dto.ExpenseFilterDTO;
 import com.qilun.expensemanager.entity.Expense;
 import com.qilun.expensemanager.service.ExpenseService;
+import com.qilun.expensemanager.validator.ExpenseValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.Valid;
 import java.math.BigDecimal;
 import java.text.ParseException;
 import java.util.List;
@@ -39,8 +42,14 @@ public class ExpenseController {
     }
 
     @PostMapping("/saveOrUpdateExpense")
-    public String saveOrUpdateExpenseDetails(@ModelAttribute("expense") ExpenseDTO expenseDTO) throws ParseException {
+    public String saveOrUpdateExpenseDetails(@Valid @ModelAttribute("expense") ExpenseDTO expenseDTO, BindingResult result) throws ParseException {
         System.out.println("Printing the Expense DTO: " +expenseDTO);
+
+        new ExpenseValidator().validate(expenseDTO, result);
+
+        if (result.hasErrors()){
+            return "expense-form";
+        }
         expenseService.saveExpenseDetails(expenseDTO);
         return "redirect:/expenses";
     }
