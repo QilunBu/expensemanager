2b24c8693a61cad6eb87b4e26e64efc99761fa5c
QilunBu <92778793+QilunBu@users.noreply.github.com>
2022-09-09 00:27:57 +1000

    completed regidter form validation

--- src/main/java/com/qilun/expensemanager/controller/AuthController.java
+++ src/main/java/com/qilun/expensemanager/controller/AuthController.java
@@ -5,9 +5,12 @@ import com.qilun.expensemanager.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
@@ -27,8 +30,11 @@ public class AuthController {
     }
 
     @PostMapping("/register")
-    public String register(@ModelAttribute("user") UserDTO userDTO, Model model){
+    public String register(@Valid @ModelAttribute("user") UserDTO userDTO, BindingResult result, Model model){
         System.out.println("printing the user details: "+userDTO);
+        if (result.hasErrors()){
+            return "register";
+        }
         userService.save(userDTO);
         model.addAttribute("successMsg", true);
         return "login";
